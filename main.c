b6bbd2a4f78813486ee3785510cb1027640bb1f4
ALojdl <andrej.lojdl@gmail.com>
2015-12-01 23:52:42 -0800

    Renamed controler to middlware. Controler draws reader to thing about this software arhitectures as MVC.

--- main.c
+++ main.c
@@ -8,35 +8,44 @@ int32_t callbackFunction(uint16_t keyPressed)
     switch (keyPressed)
     {
         case KEYCODE_P_MINUS: 
+        {
             channelDown();
             break;
-            
-        case KEYCODE_P_PLUS: 
+        }            
+        case KEYCODE_P_PLUS:
+        { 
             channelUp();
             break;
-            
+        }    
         case KEYCODE_V_PLUS: 
+        {
             volumeUp();
             break;
-            
+        }    
         case KEYCODE_V_MINUS: 
+        {
             volumeDown();
             break;
-            
+        }    
         case KEYCODE_MUTE: 
+        {
             muteVolume();
             break;
-            
+        }    
         case KEYCODE_EXIT: 
+        {
             deinitHardware();
             break;
-            
+        }    
         case KEYCODE_INFO: 
+        {
             getInfo();
             break;   
-                                                         
+        }                                                 
         default:
+        {
             goToChannel(keyPressed - 1);
+        }
     }
     
     return 0;
@@ -44,7 +53,6 @@ int32_t callbackFunction(uint16_t keyPressed)
 
 int main(int argc, char *argv[])
 {    
-    // Check if number of arguments is correct.
     if (argc == 2)
     {
         savePath(argv[1]);
---
+++ middlware.h
@@ -0,0 +1,18 @@
+#ifndef _CONTROLER_H
+#define _CONTROLER_H
+
+#include <stdint.h>
+#include "globals.h"
+
+void savePath(char *pathToConfigFile);
+void initHardware();
+void deinitHardware();
+void channelDown();
+void channelUp();
+void goToChannel(uint16_t channel);
+void volumeUp();
+void volumeDown();
+void muteVolume();
+void getInfo();
+
+#endif // _CONTROLER_H

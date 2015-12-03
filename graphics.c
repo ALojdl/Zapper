7a2ce516cdb31a78c54f2c209f8fb08cf95958b7
ALojdl <andrej.lojdl@gmail.com>
2015-12-03 03:52:49 -0800

    Changed graphics engine. Render is called only when changing something. Functions just update elements description.

--- graphics.c
+++ graphics.c
@@ -27,6 +27,24 @@ if (err != DFB_OK)                                          \
   }                                                         \
 }
 
+typedef struct _info_bar_t
+{
+    uint8_t channelNumber;
+    uint8_t teletextExist;  
+    uint8_t audioPID;
+    uint8_t videoPID;  
+    char date[11];    
+    uint8_t visible;
+} info_bar_t;
+
+typedef struct _volume_bar_t
+{
+    uint8_t volume;
+    uint8_t visible;
+} volume_bar_t;
+
+static info_bar_t infoBar;
+static volume_bar_t volumeBar;
 static IDirectFBSurface *primary = NULL;
 static IDirectFB *dfbInterface = NULL;
 static int screenWidth;
@@ -52,16 +70,16 @@ t_Error initGraphic()
     DFBCHECK(DirectFBCreate(&dfbInterface));
 	DFBCHECK(dfbInterface->SetCooperativeLevel(dfbInterface, DFSCL_FULLSCREEN));
     
-    // Create primary surface, with enabled double buffering.     
+    // Create primary surface, with enabled double buffering and fetch size.     
 	surfaceDesc.flags = DSDESC_CAPS;
 	surfaceDesc.caps = DSCAPS_PRIMARY | DSCAPS_FLIPPING;
 	DFBCHECK (dfbInterface->CreateSurface(dfbInterface,
-	    &surfaceDesc, &primary));    
-    
-    // Fetch the screen size and clear screen.
-    DFBCHECK (primary->GetSize(primary, &screenWidth, &screenHeight));   
-    DFBCHECK(primary->SetColor(primary, 0x00, 0x00, 0x00, 0x00));                               
-	DFBCHECK(primary->FillRectangle(primary, 0, 0, screenWidth, screenHeight));
+	    &surfaceDesc, &primary));   
+	DFBCHECK (primary->GetSize(primary, &screenWidth, &screenHeight));  
+	
+	// Set both elements to not visible.
+	infoBar.visible = 0;
+	volumeBar.visible = 0;  
                                     
     // Prepare volume timer specification.
 	memset(&volumeTimerSpec, 0, sizeof(volumeTimerSpec));	
@@ -106,37 +124,95 @@ t_Error initGraphic()
     return NO_ERROR;
 }
 
-t_Error drawVolume(uint8_t volume)
-{  
-	IDirectFBImageProvider *provider;
+void render()
+{
+    IDirectFBImageProvider *provider;
 	IDirectFBSurface *logoSurface;
+	IDirectFBFont *fontInterface;
+	DFBFontDescription fontDesc;
 	char imageName[15];
 	int32_t logoHeight;
 	int32_t logoWidth;
-	int32_t timerFlags = 0;	
+	char prog[32];
+	char date[32];
+	char telxt[32];
+	char audio[32];
+	char video[32];
 	
-	// Make a new name.
-	sprintf(imageName, "volume_%hu.png", volume);
+    // Fetch the screen size and clear screen.    
+    DFBCHECK(primary->SetColor(primary, 0x00, 0x00, 0x00, 0x00));                               
+	DFBCHECK(primary->FillRectangle(primary, 0, 0, screenWidth, screenHeight));
 	
-    // Read image, prepare surface descriptor and render image to given surface.
-	DFBCHECK(dfbInterface->CreateImageProvider(dfbInterface, imageName, &provider));
-	DFBCHECK(provider->GetSurfaceDescription(provider, &surfaceDesc));
-	DFBCHECK(dfbInterface->CreateSurface(dfbInterface, &surfaceDesc, &logoSurface));
-	DFBCHECK(provider->RenderTo(provider, logoSurface, NULL));
-	provider->Release(provider);
+	if (volumeBar.visible)	
+	{
+	    // Make a new name.
+	    sprintf(imageName, "volume_%hu.png", volumeBar.volume);
 	
-    // Fetch the logo size and add it to the screen buffer. 
-	DFBCHECK(logoSurface->GetSize(logoSurface, &logoWidth, &logoHeight));
-	DFBCHECK(primary->Blit(primary, logoSurface,
-	    NULL, VOLUME_X_COOR, VOLUME_Y_COOR));    
+        // Read image, prepare surface descriptor and render image to given surface.
+	    DFBCHECK(dfbInterface->CreateImageProvider(dfbInterface, imageName, &provider));
+	    DFBCHECK(provider->GetSurfaceDescription(provider, &surfaceDesc));
+	    DFBCHECK(dfbInterface->CreateSurface(dfbInterface, &surfaceDesc, &logoSurface));
+	    DFBCHECK(provider->RenderTo(provider, logoSurface, NULL));
+	    provider->Release(provider);
+	
+        // Fetch the logo size and add it to the screen buffer. 
+	    DFBCHECK(logoSurface->GetSize(logoSurface, &logoWidth, &logoHeight));
+	    DFBCHECK(primary->Blit(primary, logoSurface,
+	        NULL, VOLUME_X_COOR, VOLUME_Y_COOR));   
+    } 
+    
+    if (infoBar.visible)
+    {
+        // Prepare strings for info bar.
+	    sprintf(prog, "Program %hu", infoBar.channelNumber);
+	    sprintf(date, "Naziv dana u nedelji %s", infoBar.date);
+	    sprintf(audio, "Audio PID %hu", infoBar.audioPID);
+	    sprintf(video, "Video PID %hu", infoBar.videoPID);
+	    if (infoBar.teletextExist)
+        {
+            sprintf(telxt, "Teletekst postoji");    
+        }
+        else
+        {
+            sprintf(telxt, "Teletekst ne postoji");    
+        }
+        
+        // Prepare text font, color and size. Then create font and draw it.
+	    fontDesc.flags = DFDESC_HEIGHT;
+	    fontDesc.height = 40;
+	    DFBCHECK(dfbInterface->CreateFont(dfbInterface, 
+	        "/home/galois/fonts/DejaVuSans.ttf", &fontDesc, &fontInterface));
+	    DFBCHECK(primary->SetFont(primary, fontInterface));    
+	
+	    // Draw a rectangle representing info bar.    
+        DFBCHECK(primary->SetColor(primary, INFO_BAR_RED, 
+            INFO_BAR_GREEN, INFO_BAR_BLUE, 0xff));
+        DFBCHECK(primary->FillRectangle(primary, screenWidth/6, 4*screenHeight/5,
+            4*screenWidth/6, screenHeight/6));
+
+        // Write program number, date and teletext to info bar.
+        DFBCHECK(primary->SetColor(primary, TEXT_RED, TEXT_GREEN, TEXT_BLUE, 0xff));
+        DFBCHECK(primary->DrawString(primary, prog, -1, screenWidth/5, 
+	        17*screenHeight/20, DSTF_LEFT));
+	    DFBCHECK(primary->DrawString(primary, audio, -1, screenWidth/5, 
+	        18*screenHeight/20, DSTF_LEFT));
+	    DFBCHECK(primary->DrawString(primary, video, -1, screenWidth/5, 
+	        19*screenHeight/20, DSTF_LEFT));
+	    DFBCHECK(primary->DrawString(primary, date, -1, 4*screenWidth/9, 
+	        19*screenHeight/20, DSTF_LEFT));  
+	    DFBCHECK(primary->DrawString(primary, telxt, -1, 4*screenWidth/9, 
+	    	17*screenHeight/20, DSTF_LEFT));    
+    }
     
     // Switch buffers.
 	DFBCHECK(primary->Flip(primary, NULL, 0));
-	
-	// Fetch the logo size and add it to the second screen buffer. 
-	DFBCHECK(logoSurface->GetSize(logoSurface, &logoWidth, &logoHeight));
-	DFBCHECK(primary->Blit(primary, logoSurface,
-	    NULL, VOLUME_X_COOR, VOLUME_Y_COOR));  
+}
+
+t_Error drawVolume(uint8_t volume)
+{  
+	volumeBar.volume = volume;
+	volumeBar.visible = 1;
+	render();
     
     // Specify the timer timeout time and set it, saving the old specifications. 
     volumeTimerSpec.it_value.tv_sec = VOLUME_TIME;
@@ -154,71 +230,13 @@ t_Error drawVolume(uint8_t volume)
 
 t_Error drawInfoBar(info_data_t data)
 {
-    IDirectFBFont *fontInterface;
-	DFBFontDescription fontDesc;
-	char prog[32];
-	char date[32];
-	char telxt[32];
-	char audio[32];
-	char video[32];
-	
-	// Prepare strings for info bar.
-	sprintf(prog, "Program %hu", data.channelNumber);
-	sprintf(date, "Naziv dana u nedelji %s", data.date);
-	sprintf(audio, "Audio PID %hu", data.audioPID);
-	sprintf(video, "Video PID %hu", data.videoPID);
-	if (data.teletextExist)
-    {
-        sprintf(telxt, "Teletekst postoji");    
-    }
-    else
-    {
-        sprintf(telxt, "Teletekst ne postoji");    
-    }
-    
-    // Prepare text font, color and size. Then create font and draw it.
-	fontDesc.flags = DFDESC_HEIGHT;
-	fontDesc.height = 40;
-	DFBCHECK(dfbInterface->CreateFont(dfbInterface, 
-	    "/home/galois/fonts/DejaVuSans.ttf", &fontDesc, &fontInterface));
-	DFBCHECK(primary->SetFont(primary, fontInterface));    
-	
-	// Draw a rectangle representing info bar.    
-    DFBCHECK(primary->SetColor(primary, INFO_BAR_RED, 
-        INFO_BAR_GREEN, INFO_BAR_BLUE, 0xff));
-    DFBCHECK(primary->FillRectangle(primary, screenWidth/6, 4*screenHeight/5,
-        4*screenWidth/6, screenHeight/6));
-
-    // Write program number, date and teletext to info bar.
-    DFBCHECK(primary->SetColor(primary, TEXT_RED, TEXT_GREEN, TEXT_BLUE, 0xff));
-    DFBCHECK(primary->DrawString(primary, prog, -1, screenWidth/5, 
-	    17*screenHeight/20, DSTF_LEFT));
-	DFBCHECK(primary->DrawString(primary, audio, -1, screenWidth/5, 
-	    18*screenHeight/20, DSTF_LEFT));
-	DFBCHECK(primary->DrawString(primary, video, -1, screenWidth/5, 
-	    19*screenHeight/20, DSTF_LEFT));
-	DFBCHECK(primary->DrawString(primary, date, -1, 4*screenWidth/9, 
-	    19*screenHeight/20, DSTF_LEFT));  
-	DFBCHECK(primary->DrawString(primary, telxt, -1, 4*screenWidth/9, 
-	    17*screenHeight/20, DSTF_LEFT));
-    
-    // Switch buffers.
-	DFBCHECK(primary->Flip(primary, NULL, 0));
-	
-	// Draw a rectangle representing info bar to second buffer.    
-    DFBCHECK(primary->SetColor(primary, INFO_BAR_RED, 
-        INFO_BAR_GREEN, INFO_BAR_BLUE, 0xff));
-    DFBCHECK(primary->FillRectangle(primary, screenWidth/6, 4*screenHeight/5,
-        4*screenWidth/6, screenHeight/6));
-
-    // Write program number, date and teletext to info bar on second buffer.
-    DFBCHECK(primary->SetColor(primary, TEXT_RED, TEXT_GREEN, TEXT_BLUE, 0xff));
-    DFBCHECK(primary->DrawString(primary, prog, -1, screenWidth/5, 
-	    17*screenHeight/20, DSTF_LEFT));
-	DFBCHECK(primary->DrawString(primary, date, -1, screenWidth/5, 
-	    19*screenHeight/20, DSTF_LEFT));  
-	DFBCHECK(primary->DrawString(primary, telxt, -1, 5*screenWidth/9, 
-	    17*screenHeight/20, DSTF_LEFT));
+    infoBar.channelNumber = data.channelNumber;
+    infoBar.teletextExist = data.teletextExist;  
+    infoBar.audioPID = data.audioPID;
+    infoBar.videoPID = data.videoPID;  
+    strcpy(infoBar.date, data.date);    
+    infoBar.visible = 1;
+    render();
     
     // Specify the timer timeout time and set new timer.
     infoTimerSpec.it_value.tv_sec = INFO_TIME;
@@ -235,20 +253,8 @@ t_Error drawInfoBar(info_data_t data)
 
 void removeInfoBar()
 {		
-	// Draw transparent rectangle in place of info bar.  
-    DFBCHECK(primary->SetColor(primary, INFO_BAR_RED, 
-        INFO_BAR_GREEN, INFO_BAR_BLUE, 0x00));
-    DFBCHECK(primary->FillRectangle(primary, screenWidth/6, 4*screenHeight/5,
-        4*screenWidth/6, screenHeight/6));
-	
-	// Switch buffers.
-	DFBCHECK(primary->Flip(primary, NULL, 0));
-	
-	// Draw transparent rectangle in place of info bar on second buffer.  
-    DFBCHECK(primary->SetColor(primary, INFO_BAR_RED, 
-        INFO_BAR_GREEN, INFO_BAR_BLUE, 0x00));
-    DFBCHECK(primary->FillRectangle(primary, screenWidth/6, 4*screenHeight/5,
-        4*screenWidth/6, screenHeight/6));
+	infoBar.visible = 0;
+	render();
 	
 	// Stop timer.
     memset(&infoTimerSpec, 0, sizeof(infoTimerSpec));
@@ -260,20 +266,8 @@ void removeInfoBar()
 
 void removeVolume(union sigval signalArg)
 {	    	
-	// Draw transparent rectangle in place of volume bar.  
-    DFBCHECK(primary->SetColor(primary, INFO_BAR_RED,
-        INFO_BAR_GREEN, INFO_BAR_BLUE, 0x00));
-    DFBCHECK(primary->FillRectangle(primary, VOLUME_X_COOR, VOLUME_Y_COOR,
-        200, 200));
-	
-	// Switch buffers.
-	DFBCHECK(primary->Flip(primary, NULL, 0));
-	
-	// Draw transparent rectangle in place of volume bar on second buffer.  
-    DFBCHECK(primary->SetColor(primary, INFO_BAR_RED,
-        INFO_BAR_GREEN, INFO_BAR_BLUE, 0x00));
-    DFBCHECK(primary->FillRectangle(primary, VOLUME_X_COOR, VOLUME_Y_COOR,
-        200, 200));
+	volumeBar.visible = 0;
+	render();
 	
 	// Stop timer.
     memset(&volumeTimerSpec, 0, sizeof(volumeTimerSpec));
